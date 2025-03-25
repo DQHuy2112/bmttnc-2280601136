b77c6e8b2d11fe71f63ebded53d8349c04a9bdfe
DQHuy2112 <doquochuy2112@gmail.com>
2025-03-25 20:33:04 +0700

    Lab-03

--- Lab-03/cipher/rsa/rsa_cipher.py
+++ Lab-03/cipher/rsa/rsa_cipher.py
@@ -1,63 +1,40 @@
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import PKCS1_OAEP
-from Crypto.Signature import pkcs1_15
-from Crypto.Hash import SHA256
-import os
+import rsa, os
+
+if not os.path.exists('cipher/rsa/keys'):
+    os.makedirs('cipher/rsa/keys')
 
 class RSACipher:
     def __init__(self):
-        self.private_key_path = "private_key.pem"
-        self.public_key_path = "public_key.pem"
+        pass
 
     def generate_keys(self):
-        """Tạo cặp khóa RSA và lưu vào file."""
-        key = RSA.generate(2048)
-        private_key = key.export_key()
-        public_key = key.publickey().export_key()
-
-        with open(self.private_key_path, "wb") as priv_file:
-            priv_file.write(private_key)
-
-        with open(self.public_key_path, "wb") as pub_file:
-            pub_file.write(public_key)
+        (public_key, private_key) = rsa.newkeys(1024)
+        with open('cipher/rsa/keys/publicKey.pem', 'wb') as p:
+            p.write(public_key.save_pkcs1('PEM'))
+        with open('cipher/rsa/keys/privateKey.pem', 'wb') as p:
+            p.write(private_key.save_pkcs1('PEM'))
 
     def load_keys(self):
-        """Tải khóa RSA từ file."""
-        if not os.path.exists(self.private_key_path) or not os.path.exists(self.public_key_path):
-            raise FileNotFoundError("Khóa RSA chưa được tạo. Vui lòng gọi phương thức generate_keys trước.")
-
-        with open(self.private_key_path, "rb") as priv_file:
-            private_key = RSA.import_key(priv_file.read())
-
-        with open(self.public_key_path, "rb") as pub_file:
-            public_key = RSA.import_key(pub_file.read())
-
+        with open('cipher/rsa/keys/publicKey.pem', 'rb') as p:
+            public_key = rsa.PublicKey.load_pkcs1(p.read())
+        with open('cipher/rsa/keys/privateKey.pem', 'rb') as p:
+            private_key = rsa.PrivateKey.load_pkcs1(p.read())
         return private_key, public_key
 
     def encrypt(self, message, key):
-        """Mã hóa thông điệp bằng khóa RSA."""
-        cipher = PKCS1_OAEP.new(key)
-        encrypted_message = cipher.encrypt(message.encode("utf-8"))
-        return encrypted_message
+        return rsa.encrypt(message.encode('ascii'), key)
 
     def decrypt(self, ciphertext, key):
-        """Giải mã thông điệp bằng khóa RSA."""
-        cipher = PKCS1_OAEP.new(key)
-        decrypted_message = cipher.decrypt(ciphertext)
-        return decrypted_message.decode("utf-8")
-
-    def sign(self, message, private_key):
-        """Ký thông điệp bằng khóa riêng."""
-        h = SHA256.new(message.encode("utf-8"))
-        signature = pkcs1_15.new(private_key).sign(h)
-        return signature
-
-    def verify(self, signature, message, public_key):
-        """Xác minh chữ ký bằng khóa công khai."""
-        h = SHA256.new(message.encode("utf-8"))
         try:
-            pkcs1_15.new(public_key).verify(h, signature)
-            return True
-        except (ValueError, TypeError):
+            return rsa.decrypt(ciphertext, key).decode('ascii')
+        except:
             return False
 
+    def sign(self, message, key):
+        return rsa.sign(message.encode('ascii'), key, 'SHA-1')
+
+    def verify(self, message, signature, key):
+        try:
+            return rsa.verify(message.encode('ascii'), signature, key,) == 'SHA-1'
+        except:
+            return False
--- Lab-03/ui/caesar.py
+++ Lab-03/ui/caesar.py
@@ -10,7 +10,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import os
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = 'C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages\\PyQt5\\Qt\\plugins\\platforms'
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = 'C:\\Users\\THUY HANG\\AppData\\Local\\Programs\\Python\\Python39\\Lib\\site-packages\\PyQt5\\Qt\\plugins\\platforms'
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
